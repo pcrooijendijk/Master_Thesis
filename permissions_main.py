5ee3932ca1847f9d8be3c18589778765e515edc2
prooijendijk <pien.rooijendijk@foxcrypto.com>
2025-02-19 16:38:07 +0100

    Implemented the logic for the Clients, also using the permissions

--- permissions_main.py
+++ permissions_main.py
@@ -1,34 +1,34 @@
-import os
 import pymupdf
 import random
+from pathlib import Path
 
 import Space
 import Permission
-from UserPermissions import UserAccessor
 import TransactionTemplate
+from client import Client
+from UserPermissions import UserAccessor
 from SpacePermissions import SpaceManager
 from SpacePermissions import SpacePermissionManager
 from UserPermissions import UserManager
 from UserPermissionManagement import UserPermissionsResource
 
 
-file_path = "/media/sf_thesis/data_DocBench_test"
+file_path = Path("/media/sf_thesis/data_DocBench_test")
 texts = []
 
 # Append the documents to texts lists to get the content of the documents
-for x in os.listdir(file_path):
-    for y in os.listdir(file_path + "/" + x):
-        if y.endswith(".pdf"):
-            with pymupdf.open(file_path + "/" + x + "/" + y) as doc:
-                space_key_index = random.randint(0, 3)  # For the space key
-                texts.append(
+for pdf in file_path.rglob("*.pdf"): # Use rglob to find all PDFs
+    with pymupdf.open(pdf) as file: 
+        space_key_index = random.randint(0, 3)  # For the space key
+        texts.append(
                     [
-                        chr(12).join([page.get_text() for page in doc]),
-                        doc.metadata,
+                        chr(12).join([page.get_text() for page in file]),
+                        file.metadata,
                         space_key_index,
                     ]
                 )
 
+# Template for role permissions (not mandatory to use these)
 role_permissions = {
     "admin": list(Permission.Permission),  # Full access with all permissions
     "editor": [Permission.Permission.VIEWSPACE_PERMISSION, Permission.Permission.CREATEEDIT_PAGE_PERMISSION, Permission.Permission.COMMENT_PERMISSION],
@@ -39,18 +39,21 @@ role_permissions = {
 # Making a space, space keys are defined (for now) as integers
 space = Space.Space('mark', 0)
 space_new = Space.Space('new', 1)
+space_3 = Space.Space("lol", 2)
 
 # Adding two documents to the space
 space.add_document(texts[0]) 
 space.add_document(texts[1])
 space_new.add_document(texts[2])
 space_new.add_document(texts[3])
+space_3.add_document(texts[2])
 
 # Make a space manager
 space_manager = SpaceManager()
 # Adding the spaces
 space_manager.add_space(space) 
 space_manager.add_space(space_new) 
+space_manager.add_space(space_3)
 
 # Adding users
 user_accessor = UserAccessor()
@@ -65,13 +68,19 @@ user_manager.add_admin("admin")
 space_permission_manager = SpacePermissionManager()
 
 # Adding permissions for admin (space, username, permission type)
-space_permission_manager.save_permission(space, 'admin', role_permissions["admin"])
+space_permission_manager.save_permission(space, 'admin', role_permissions["editor"])
 space_permission_manager.save_permission(space_new, 'user1', role_permissions["viewer"])
+space_permission_manager.save_permission(space_3, 'admin', role_permissions["editor"])
 
 # Getting the permissions for user with admin permissions
 user_permissions_resource = UserPermissionsResource(user_manager, TransactionTemplate.TransactionTemplate(), user_accessor, space_manager, space_permission_manager)
 
 # Get the permissions for user admin (target username, request)
-print(user_permissions_resource.get_permissions('admin', {"Username": "admin"}))
-print("----------------------------------------------------------")
-print(user_permissions_resource.get_permissions('user1', {"Username": "user1"}))
+# print(user_permissions_resource.get_permissions('admin', {"Username": "admin"}))
+# print("----------------------------------------------------------")
+# print(user_permissions_resource.get_permissions('user1', {"Username": "user1"}))
+
+# Make the clients by using the Client class:
+admin_client = Client(1, "admin", user_permissions_resource, "DeepSeek-v3:1.5b")
+admin_client.get_spaces()
+admin_client.filter_documents()
