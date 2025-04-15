f7fb4c996fb850ffa861324aeff3b1684521fe49
pcrooijendijk <pien.rooijendijk@yahoo.com>
2025-04-15 13:29:28 +0000

    Making peft model

--- fed_utils/Server.py
+++ fed_utils/Server.py
@@ -2,14 +2,51 @@ import torch
 import os
 import gc
 from torch.nn.functional import normalize
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
 from peft import (
+    LoraConfig,
+    get_peft_model,
     set_peft_model_state_dict,
+    prepare_model_for_kbit_training,
 )
 
 class Server:
     def __init__(self, num_clients, global_model):
         self.global_model = global_model
         self.num_clients = num_clients
+    
+    def setting_peft_model(self) -> None:
+        model = AutoModelForCausalLM.from_pretrained(
+            self.global_model,
+            load_in_8bit=True,
+            torch_dtype=torch.float16,
+            device_map="auto",
+            )
+
+        tokenizer = AutoTokenizer.from_pretrained(self.global_model)
+        tokenizer.pad_token_id = (
+            0
+        )
+        tokenizer.padding_side = "left"
+
+        # Using this technique to reduce memory-usage and accelarting inference
+        model = prepare_model_for_kbit_training(model) 
+
+        # Initialize LoRA
+        lora_config = LoraConfig(
+            r=16, 
+            lora_alpha=16, 
+            target_modules=[ # The layers which need to be finetuned
+                "q_proj",
+            ], 
+            lora_dropout=0.05, 
+            bias="none",
+            task_type="CAUSAL_LM"
+        )
+
+        # Get the PEFT model using LoRA
+        self.model = get_peft_model(model, lora_config)
     
     def FedAvg(self, model, selected_clients, dataset_length, epoch, output_dir):
         # Normalizing the weights of each client
@@ -25,6 +62,9 @@ class Server:
 
             # Load weights on CPU, not GPU
             weights = torch.load(total_output_dir, map_location=torch.device("cpu"))
+
+            for k in weights: 
+                weights[k] = weights[k].float()
 
             if k == 0:
                 weighted_weights = {key: weights[key] * weights_array[k] for key in weights}
@@ -45,6 +85,8 @@ class Server:
         for key in weighted_weights:
             weighted_weights[key] = weighted_weights[key].float().cpu()
 
-        set_peft_model_state_dict(model, weighted_weights, "default")
+        self.setting_peft_model()    
+
+        set_peft_model_state_dict(self.model, weighted_weights, "default")
 
         return model
