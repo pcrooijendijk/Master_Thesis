40e87c86f565665418cd980b659aa1437f8d2111
pcrooijendijk <pien.rooijendijk@yahoo.com>
2025-04-17 09:45:25 +0000

    Printing response

--- inference.py
+++ inference.py
@@ -59,6 +59,7 @@ def run(
         # If there are no documents uploaded, generate a prompt without extra context
         else:
             response = deepseek.generate_response(question, deepseek, top_k, top_p, num_beams, max_new_tokens, 0.0, temp, False)
+        print(response)
         return response['content'], metadata
 
     # The Gradio interface for fetching the question, documents, custom input and parameters
