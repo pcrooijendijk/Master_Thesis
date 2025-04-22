bd0b2a6cb9acb1e58b5c9cc58c8caf442dcde2b6
pcrooijendijk <pien.rooijendijk@yahoo.com>
2025-04-22 13:51:47 +0000

    Remove whitespaces

--- DeepSeek/deepseek_application.py
+++ DeepSeek/deepseek_application.py
@@ -281,11 +281,11 @@ class DeepSeekApplication:
             context_documents = self.retrieve_relevant_docs(query, top_k, similarity_threshold)
             
             # Truncate context if it is too long
-            combined_context = ' '.join(context_documents)
+            combined_context = ' '.strip()
             # if len(combined_context) > max_context_length:
             #     combined_context = combined_context[:max_context_length] + "..."
             
-            prompt = self.construct_prompt(query, context_documents)
+            prompt = self.construct_prompt(query, combined_context)
             print("prompt", prompt)
             inputs = deepseek.tokenizer(prompt, return_tensors="pt")
             input_ids = inputs["input_ids"].to(device)
