acc6b56063c802a89f616aa9bdd7dc49711fb4fb
pcrooijendijk <pien.rooijendijk@yahoo.com>
2025-04-22 12:11:32 +0000

    More testing

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
 
-loaders = TextLoader("utils/documents.json")
+loader = TextLoader("utils/documents.json")
+documents = loader.load()
+print("documents", documents)
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
