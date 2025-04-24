a5fbe219638be2d4c3ddec8f8eee2af9080719e6
Pien Rooijendijk <prooijendijk@slpc08.science.ru.nl>
2025-04-24 15:20:48 +0200

    Adjusted the prompting

--- DeepSeek/deepseek_application.py
+++ DeepSeek/deepseek_application.py
@@ -301,8 +301,7 @@ class DeepSeekApplication:
                 "You are an assistant for question-answering tasks. "
                 "Use the following pieces of retrieved context to answer "
                 "the question. If you don't know the answer, say that you "
-                "don't know. Use three sentences maximum and keep the "
-                "answer concise."
+                "don't know. "
                 "\n\n"
                 "{context}"
             )
