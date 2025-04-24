0bde14c63b3fe6c7bc503d2c3bf52d311529a377
Pien Rooijendijk <prooijendijk@slpc08.science.ru.nl>
2025-04-24 15:35:07 +0200

    Changed client

--- inference.py
+++ inference.py
@@ -8,7 +8,7 @@ from DeepSeek import DeepSeekApplication, Metadata
 OUTPUT_HISTORY = []
 
 def run(
-    client_id: int = 9,    
+    client_id: int = 2,    
     ori_model: str = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", # The original model 
     lora_weights_path: str = "FL_output/pytorch_model.bin", # Path to the weights after LoRA
     lora_config_path: str = "FL_output", # Path to the config.json file after LoRA
