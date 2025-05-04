b2e24e3e444cc33134dfb22bbf2c7e93c1d064c8
pcrooijendijk <pien.rooijendijk@yahoo.com>
2025-05-04 12:13:33 +0000

    Change boolean

--- DeepSeek/deepseek_application.py
+++ DeepSeek/deepseek_application.py
@@ -269,6 +269,7 @@ class DeepSeekApplication:
                 self.uploaded_doc_present = True
                 self.documents_array = loading_documents(documents, documents_array, dict=False) # Adding additional documents to the chunks
             else: 
+                self.uploaded_doc_present = False
                 all_documents = load_dataset("json", data_files="utils/documents.json")
                 temp_doc = []
                 for index, _ in enumerate(all_documents['train']):
