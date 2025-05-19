3f044e3a26dce2ea5546ca81e3dd52d645214f73
pcrooijendijk <pien.rooijendijk@yahoo.com>
2025-05-19 15:03:02 +0000

    new test langchain

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 
 # loading the V2 dataset
-amnesty_qa = load_dataset("explodinggradients/amnesty_qa", "english_v2")
+amnesty_qa = load_dataset("explodinggradients/amnesty_qa", "english_v2", trust_remote_code=True)
 
 amnesty_subset = amnesty_qa["eval"].select(range(2))
 
