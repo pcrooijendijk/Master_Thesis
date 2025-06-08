fa187248c9d49e11e6cb6f8a6da64c0502564827
pcrooijendijk <pien.rooijendijk@yahoo.com>
2025-06-08 20:11:23 +0200

    Changed input parsing for evaluation for eval argument

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
-from ragas.metrics import RougeScore, BleuScore
-from ragas import SingleTurnSample
-from datasets import load_dataset
+import os
+import json
+import sys
+from typing import Optional
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--path", help="Put in the path to the evaluation dataset.")
-args = parser.parse_args()
-print(args.path)
+from datasets import load_dataset, Dataset, DatasetDict
+from ragas import evaluate, RunConfig
+from ragas.metrics import answer_relevancy, faithfulness, context_recall, context_precision
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
 
-# Loading the dataset
-all_documents = load_dataset("json", data_files=args.path)["train"]
-print(all_documents)
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", help="Choose evaluation mode. Use --mode full or --mode base", required=True, default="full")
+parser.add_argument("--client_id", help="Choose which client to evaluate.", required=True, default=2)
+parser.add_argument("eval", help="Whether to perform evaluation with RAGAS.")
+args = parser.parse_args()
+
+client_id: int = args.client_id
+print(client_id)
+
+# ------------------------------------------------------------------------------------------------------
+if args.eval:
+    print("heeeyy")
