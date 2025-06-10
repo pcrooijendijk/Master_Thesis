f8ab56df6c8442f35d2c94d15739e71684db41ab
pcrooijendijk <pien.rooijendijk@yahoo.com>
2025-06-10 11:55:05 +0200

    Computed scores and it is working :)

--- test/ixn_score.py
+++ test/ixn_score.py
@@ -1,10 +1,11 @@
 from typing import List, Dict
 import json
 import argparse
+import pickle
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--path1", help="Put in the path of the first retrieved documents dataset.", required=True)
-parser.add_argument("--path2", help="Put in the path of the second retrieved documents dataset.", required=True)
+parser.add_argument("--path", help="Put in the path of the retrieved documents dataset.", required=True)
+parser.add_argument("--output", help="Output CSV filename", default="ixn_scores.pickle")
 args = parser.parse_args()
 
 def compute_ixn_score(
@@ -20,6 +21,7 @@ def compute_ixn_score(
 def evaluate_ixn_for_users(
     baseline: List[List[str]],
     comparison: List[List[str]],
+    output_file: str,
     k: int = 10
 ) -> Dict[str, float]:
     ixn_scores = []
@@ -29,12 +31,13 @@ def evaluate_ixn_for_users(
 
     avg_score = round(sum(ixn_scores) / len(ixn_scores), 4)
     print("IXN scores:", ixn_scores)
-    return {"avg_ixn": avg_score}
+    with open(output_file, 'wb') as f:
+        pickle.dump(ixn_scores, f)
 
-with open(args.path1) as f: 
-    revelant_documents_9 = json.load(f)
+with open("retrieved_docs/retrieved_docs_baseline.json") as f: 
+    baseline_docs = json.load(f)
 
-with open(args.path2) as f: 
-    revelant_documents_2 = json.load(f)
+with open(args.path) as f: 
+    revelant_documents = json.load(f)
 
-evaluate_ixn_for_users(revelant_documents_9, revelant_documents_2)
+evaluate_ixn_for_users(baseline_docs, revelant_documents, args.output)
